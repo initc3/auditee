41a6eca6311314d4fa5dc4e07b9e4a91ea62c7e6
Sylvain Bellemare <sbellem@gmail.com>
2021-03-12 01:26:58 +0000

    wip

--- auditee/bindings/quote.py
+++ auditee/bindings/quote.py
@@ -1,4 +1,5 @@
 import pathlib
+from base64 import b64decode
 
 from cffi import FFI
 
@@ -14,4 +15,20 @@ def read_sgx_quote_bytes(quote_bytes):
     """Does not read the signature ... having problems reading it."""
     sgx_quote_t = ffi.new("sgx_quote_t*")
     ffi.buffer(sgx_quote_t)[:436] = quote_bytes[:436]
+    return sgx_quote_t
+
+
+def read_sgx_quote_b64(quote_b64):
+    """Does not read the signature ... having problems reading it."""
+    quote_bytes = b64decode(quote_b64)
+    sgx_quote_t = ffi.new("sgx_quote_t*")
+    ffi.buffer(sgx_quote_t)[:436] = quote_bytes[:436]
+    return sgx_quote_t
+
+
+def read_sgx_quote_body_b64(quote_body_b64):
+    """The quote body is the quote without the signature len and signature."""
+    quote_body_bytes = b64decode(quote_body_b64)
+    sgx_quote_t = ffi.new("sgx_quote_t*")
+    ffi.buffer(sgx_quote_t)[:432] = quote_body_bytes
     return sgx_quote_t
